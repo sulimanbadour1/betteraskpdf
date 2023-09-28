a03b59681b343f9596d878779dac152169971601
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-09-28 21:45:53 +0200

    update some divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 // import Image from "next/image";
-// import logo from "../assets/logos/png/logo-no-background.png";
-
+import logo from "../assets/logos/png/logo-no-background.png";
+// import Image from "next/image"; Image shit
+// <Image src={logo} alt="logo" className="object-cover" />
 export default async function Home() {
   return (
     <div className="w-screen min-h-screen bg-gradient-to-t from-gray-200 via-gray-300 to-gray-500">
@@ -9,7 +10,6 @@ export default async function Home() {
         <div className="flex flex-col text-center items-center ">
           <div className="flex text-center">
             <h1 className="mr-3 text-5xl font-semibold">Better Ask PDF</h1>
-            <h2 className=" flex flex-col text-5xl font-semibold">🤔</h2>
           </div>
         </div>
       </div>
