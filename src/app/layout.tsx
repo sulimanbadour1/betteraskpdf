85c87fad8e63a9d7a779c84a71e87a5e543e50c1
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-09-30 18:07:11 +0200

    created query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 // clerk stuff
@@ -18,9 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+      <Providers>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </Providers>
     </ClerkProvider>
   );
 }
